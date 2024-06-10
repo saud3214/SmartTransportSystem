774ddb0e69ca9b0a18d784df4b317cd74640d702
saud3214 <abbasisaud8@gmail.com>
2024-06-10 17:37:09 +0500

    v7

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   basePath: process.env.BASEPATH,
   env: {
     NEXTAUTH_URL: 'https://smart-transport-system.vercel.app/api/auth',
-    NEXTAUTH_SECRET: 'e0375c5b670f728d82a24a08a1cfcda7',
+    NEXTAUTH_SECRET: '184648cb62dbe828800dcab2b729625a',
 },
   redirects: async () => {
     return [
