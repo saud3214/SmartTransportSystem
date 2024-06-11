7050eabc79897c0bc0c9de196e61c61624251611
saud3214 <abbasisaud8@gmail.com>
2024-06-11 10:52:37 +0500

    v9

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.BASEPATH,
+  basePath: process.env.BASEPATH || '',
 
   redirects: async () => {
     return [
@@ -14,7 +14,19 @@ const nextConfig = {
   },
 
   // TODO: below line is added to resolve twice event dispatch in the calendar reducer
-  reactStrictMode: false
+  reactStrictMode: false,
+
+  env: {
+    NEXT_PUBLIC_APP_URL: process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}${process.env.BASEPATH}`
+      : `http://localhost:3000${process.env.BASEPATH}`,
+    NEXTAUTH_URL: process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}${process.env.BASEPATH}/api/auth`
+      : `http://localhost:3000${process.env.BASEPATH}/api/auth`,
+    NEXT_PUBLIC_API_URL: process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}${process.env.BASEPATH}/api`
+      : `http://localhost:3000${process.env.BASEPATH}/api`
+  }
 }
 
 module.exports = nextConfig
